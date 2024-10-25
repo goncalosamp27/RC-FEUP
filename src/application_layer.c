e2087829cce86b9cf5aaeed447bb0de516b8e0d7
goncalosamp27 <goncalo20.sampaio03@gmail.com>
2024-10-26 00:56:31 +0100

    app layer started

--- src/application_layer.c
+++ src/application_layer.c
@@ -30,9 +30,24 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate, in
 
 	switch (linklayer.role) {
 		case LlTx: {
-			// vamos ter de ler o ficheiro (do PC), enviá-lo e fechar a ligação		
-		}
+			// vamos ter de ler o ficheiro (do PC), enviá-lo e fechar a ligação
+			FILE* image = fopen(filename, "rb"); // o rb le byte a byte sem alterar nada no ficheiro
+			if(image == NULL) {
+				perror("No file was found with the name you provided\n");
+				exit(-1);
+			}
+
+			int initial_pos = ftell(image);
+			fseek(image, 0L, SEEK_END); // mover 0 bytes da posição inicial (em long) e procurar a posição final (SEEK_END)
+			int last_pos = ftell(image);
+
+			long int imageSize = last_pos - initial_pos;
+			fseek(image, initial_pos, SEEK_SET);
+
+			
+
 			break;
+		}
 		
 		case LlRx:{
 			// vamos ter de ler o ficheiro (da ligação), escrevê-lo e fechar a ligação
@@ -40,5 +55,10 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate, in
 		
 		default:
 			exit(-1);
+			break;
 	}
+}
+
+unsigned char * createControlPack(unsigned int control_field, const char * filename, long int length, unsigned int * size) {
+
 }
