0bfd58c2511afe2a2b3da1d9e68cbd88024c1d09
goncalosamp27 <goncalo20.sampaio03@gmail.com>
2024-10-26 01:53:04 +0100

    app layer control packets done

--- src/application_layer.c
+++ src/application_layer.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate, int nTries, int timeout, const char *filename) {
 	// LinkLayer struct creation
@@ -32,6 +33,7 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate, in
 		case LlTx: {
 			// vamos ter de ler o ficheiro (do PC), enviá-lo e fechar a ligação
 			FILE* image = fopen(filename, "rb"); // o rb le byte a byte sem alterar nada no ficheiro
+
 			if(image == NULL) {
 				perror("No file was found with the name you provided\n");
 				exit(-1);
@@ -44,7 +46,53 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate, in
 			long int imageSize = last_pos - initial_pos;
 			fseek(image, initial_pos, SEEK_SET);
 
+			// start control packet
+
+			int L1 = (int) ceil(log2f((float)imageSize)/8.0);// size of file
+			int L2 = strlen(filename);
+			unsigned int * size = 5 + L1 + L2; // 5 -> control + TL1 + TL2
+
+			unsigned char *packet_start;
+			unsigned int pos = 0;
+
+			packet_start[pos++] = 1;
+			packet_start[pos++] = 0;
+			packet_start[pos++] = L1;
+			long int temp_imgsize = imageSize;
+
+			for (unsigned char i = 0 ; i < L1 ; i++) {
+				packet_start[2+L1-i] = temp_imgsize & 0xFF;
+				temp_imgsize >>= 8;
+				pos++;
+    		}
 			
+			packet_start[pos++] = 1;
+			packet_start[pos++] = L2;
+			memcpy(packet_start + pos, filename, L2);
+
+			// data packet
+
+			
+
+			// end control packet
+
+			unsigned char *packet_end;
+			unsigned int pos2 = 0;
+			packet_end[pos2++] = 3; 
+			packet_end[pos2++] = 0;
+			packet_end[pos2++] = L1;
+
+			long int temp_imgsize2 = imageSize;
+
+			for (unsigned char i = 0 ; i < L1 ; i++) {
+				packet_end[2+L1-i] = temp_imgsize2 & 0xFF;
+				temp_imgsize2 >>= 8;
+				pos2++;
+    		}
+			
+			packet_end[pos2++] = 1;
+			packet_end[pos2++] = L2;
+			memcpy(packet_end + pos2, filename, L2);
 
 			break;
 		}
